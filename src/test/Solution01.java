9eebe3774e45a17464d0e27d0fdd58eae5a27af5
khadim sarr <ksarr@gainde2000.sn>
2021-04-18 15:55:47 +0000

    new code

--- src/test/Solution01.java
+++ src/test/Solution01.java
@@ -1,6 +1,9 @@
 package test;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution01 {
@@ -9,13 +12,57 @@ public class Solution01 {
 		 // Write your code here
 		 // To debug: System.err.println("Debug messages...");
 		  String resultat="";
+		  List<String> liste = new ArrayList();
+		
 		for (int i = 0; i < parts.length; i++) {
 		  
+			//resultat=resultat.concat(parts[i]);
 			
-			resultat=resultat.concat(parts[i]);
+			  liste.add(parts[i]);
 			
-			System.out.println(parts[i]);
 		}
+		
+		
+	       Collections.sort(liste);
+	       resultat=liste.get(0);
+		
+		//System.out.println("resultat"+liste.get(0));
+		
+		for (int i = 1; i < liste.size(); i++) {
+		
+			String last = resultat.substring(resultat.length()-1); // avec substring
+				
+			String fist=liste.get(i).substring(0, 1); // avec substring;
+			
+			//System.out.println("-----last-"+last+"---------fist"+fist);
+			
+			if(last.equals(fist))
+			{
+				
+		   //  System.out.println("------Resultat-------"+resultat.substring(0,resultat.length()-1));
+		      
+		      
+		      resultat= resultat.substring(0,resultat.length()-1).concat(liste.get(i)) ;
+				
+			}else 
+			{
+				
+				resultat=resultat.concat(liste.get(i));
+			}
+			
+			
+		
+			
+			 // liste.add(parts[i]);
+			
+		}
+		
+		
+		
+		
+		
+	//	System.out.println(parts[i]);
+		
 		  
 		
 		 return resultat;
